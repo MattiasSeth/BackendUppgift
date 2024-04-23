820b9d01f03ade9653169cd53d6a2aca12e4b50b
Mattias Seth <mattias.seth@gmail.com>
2024-04-23 09:10:35 +0200

    Added lombok.Data

--- src/main/java/com/example/backenduppgift/Repositories/BookingRepository.java
+++ src/main/java/com/example/backenduppgift/Repositories/BookingRepository.java
@@ -4,4 +4,5 @@ import com.example.backenduppgift.Entities.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
 }
