47e0d12889640a8bed22861ddb82586e7f28462b
Mattias Seth <mattias.seth@gmail.com>
2024-04-23 06:58:23 +0200

    Added lombok.Data

--- src/main/java/com/example/backenduppgift/Entities/Booking.java
+++ src/main/java/com/example/backenduppgift/Entities/Booking.java
@@ -5,12 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.awt.print.Book;
 import java.time.LocalDate;
 
 @Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Booking {
