b2e4dd4be5c6b34af9c89ddb197670234f022dfc
Hadi-bahrami <bahrami@hotmail.com>
2024-04-24 21:47:41 +0200

    Fixat till CustomerController + RoomController

--- src/main/java/com/example/backenduppgift/Controllers/CustomerController.java
+++ src/main/java/com/example/backenduppgift/Controllers/CustomerController.java
@@ -1,11 +1,15 @@
 package com.example.backenduppgift.Controllers;
 
+import com.example.backenduppgift.DTO.DetailedBookingDto;
 import com.example.backenduppgift.Services.CustomerService;
 import com.example.backenduppgift.DTO.CustomerDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,6 +19,18 @@ public class CustomerController {
 
     @RequestMapping("customer")
     public List<CustomerDto> getAllCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    @PostMapping("customer/add")
+    public List<CustomerDto> addCustomerDto(@RequestBody CustomerDto customerDto){
+        customerService.getAllCustomers().add(customerDto);
+        return customerService.getAllCustomers();
+    }
+
+    @RequestMapping("customer/delete")
+    public List<CustomerDto> deleteCustomerDto(@RequestBody CustomerDto customerDto){
+        customerService.getAllCustomers().remove(customerDto);
         return customerService.getAllCustomers();
     }
 }
--- src/main/java/com/example/backenduppgift/Controllers/RoomController.java
+++ src/main/java/com/example/backenduppgift/Controllers/RoomController.java
@@ -1,10 +1,13 @@
 package com.example.backenduppgift.Controllers;
 
+import com.example.backenduppgift.DTO.CustomerDto;
 import com.example.backenduppgift.DTO.RoomDto;
 import com.example.backenduppgift.Entities.Room;
 import com.example.backenduppgift.Repositories.RoomRepository;
 import com.example.backenduppgift.Services.RoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +21,22 @@ public class RoomController {
 
     private final RoomService roomService;
 
-    @RequestMapping("Rooms")
+    @RequestMapping("rooms")
     public List<RoomDto> getAllRoomsDto(){
         return roomService.getAllRoomDto();
     }
+
+    @PostMapping("rooms/add")
+    public List<RoomDto> addRoomDto(@RequestBody RoomDto roomDto){
+        roomService.getAllRoomDto().add(roomDto);
+        return roomService.getAllRoomDto();
+    }
+
+    @RequestMapping("rooms/delete")
+    public List<RoomDto> deleteRoomDto(@RequestBody RoomDto roomDto){
+        roomService.getAllRoomDto().remove(roomDto);
+        return roomService.getAllRoomDto();
+    }
+
+
 }
