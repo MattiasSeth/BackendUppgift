c240407d493e415cbc9d9922491facce186312f3
Mattias Seth <mattias.seth@gmail.com>
2024-04-27 14:45:23 +0200

    Added some customers and rooms the database

--- src/main/java/com/example/backenduppgift/BackendUppgiftApplication.java
+++ src/main/java/com/example/backenduppgift/BackendUppgiftApplication.java
@@ -25,14 +25,46 @@ public class BackendUppgiftApplication {
                                    BookingRepository bookingRepository){
         return (args ) -> {
 
+            // Customers
             Customer c1 = new Customer("Seth");
-            Room r1 = new Room("Double",2);
-
+            Customer c2 = new Customer("Gustav");
+            Customer c3 = new Customer("Edgar");
+            Customer c4 = new Customer("Hadi");
+            Customer c5 = new Customer("Nadia");
             customerRepository.save(c1);
-            roomRepository.save(r1);
+            customerRepository.save(c2);
+            customerRepository.save(c3);
+            customerRepository.save(c4);
+            customerRepository.save(c5);
 
+            // Rooms
+            Room r1 = new Room("Single",0);
+            Room r2 = new Room("Single",0);
+            Room r3 = new Room("Single",0);
+            Room r4 = new Room("Single",0);
+            Room r5 = new Room("Double",1);
+            Room r6 = new Room("Double",1);
+            Room r7 = new Room("Double",1);
+            Room r8 = new Room("Double",2);
+            Room r9 = new Room("Double",2);
+            Room r10 = new Room("Double",2);
+
+            roomRepository.save(r1);
+            roomRepository.save(r2);
+            roomRepository.save(r3);
+            roomRepository.save(r4);
+            roomRepository.save(r5);
+            roomRepository.save(r6);
+            roomRepository.save(r7);
+            roomRepository.save(r8);
+            roomRepository.save(r9);
+            roomRepository.save(r10);
+
+            // Dates
             LocalDate startDate = LocalDate.of(2024, 4, 22);
             LocalDate endDate = LocalDate.of(2024, 4, 30);
+
+            // Bookings
             bookingRepository.save(new Booking(c1,r1,0,startDate,endDate));
         };
     }
