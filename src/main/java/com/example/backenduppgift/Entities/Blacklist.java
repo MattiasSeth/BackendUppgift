962357d2134e60bbc32c01fc18bb9cfa2b3c2346
Hadi-bahrami <bahrami@hotmail.com>
2024-05-14 08:03:52 +0200

    Validering av Blacklist.java

--- src/main/java/com/example/backenduppgift/Entities/Blacklist.java
+++ src/main/java/com/example/backenduppgift/Entities/Blacklist.java
@@ -3,6 +3,8 @@ package com.example.backenduppgift.Entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,8 +24,14 @@ public class Blacklist {
     private Long id;  // database ID
 
     private int externalId;  // json ID
+
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Invalid email-address, the email needs to be a real email address" )
     private String email;
+
+    @NotEmpty(message = "Name cannot be empty")
     private String name;
+
     private String customerGroup;
     private Date created;
     private boolean ok;
