70c033bf89e4fd927f6ae867979339986f6a023d
Nadia Nazari <nadia.nazari@yh.nackademin.se>
2024-05-15 14:10:07 +0200

    Nadia: Gui visar rooms

---
+++ src/main/java/com/example/backenduppgift/Controllers/RoomController.java
@@ -0,0 +1,35 @@
+package com.example.backenduppgift.Controllers;
+
+import com.example.backenduppgift.DTO.BlacklistDto;
+import com.example.backenduppgift.DTO.RoomDto;
+import com.example.backenduppgift.Entities.Blacklist;
+import com.example.backenduppgift.Services.BlacklistService;
+import com.example.backenduppgift.Services.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(path = "/room")
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @RequestMapping("/all")
+    public String getAllCustomers(Model model){
+        List<RoomDto> rooms = roomService.getAllRooms();
+        model.addAttribute("allRooms", rooms);
+        model.addAttribute("roomTitle", "All Rooms");
+        return "showAllRooms";
+    }
+
+
+}
